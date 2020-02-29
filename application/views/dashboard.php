590c54df4494859e3994e28f372502078ca57f3a
Carlos Soto <r.carlos.soto@hotmail.com>
2020-02-28 23:41:25 -0400

    08.02.2020 dashboard

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,11 +1,75 @@
-<div class="row">
-    <div class="col-md-12">
-        <div class="box">
+<div class="row" >
+    <div class="col-md-12" >
+<!--        <div class="box">
             <div class="box-header">
                 <h3 class="box-title">
-                Please navigate to the appropriate controller / action to open the associated function with your generated code.
+                    
+                    
+                    
                 </h3>
+                
             </div>
-        </div>
+        </div>-->
+        <center>
+            
+            <a href="<?php echo base_url("lectura/lecturas"); ?>" style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."lecturas.png"; ?>" >                                              
+                    </a>
+            
+                    <a href="" style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."facturacion.png"; ?>" >                                              
+                    </a>
+                    <a href=""  style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."ingresos.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."egresos.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."ventas.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0; background: yellow" class="btn btn-facebook">
+                        <img src="<?php echo base_url("resources/images/system/")."reportes.png"; ?>" >                                              
+                    </a>
+        </center>
+    </div>
+    <div class="col-md-12" >
+        <center>
+            <h1 style="font-family: Arial;">                
+                AQUI VA UN GRAFICO
+            </h1>
+            
+        </center>
+    </div>
+    
+    <div class="col-md-12" >
+        <center>
+            
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."asociados.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."aportes.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."multas.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."parametros.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."dosificaciones.png"; ?>" >                                              
+                    </a>
+                    <a href="" style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."usuarios.png"; ?>" >                                              
+                    </a>
+<!--                    <button style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."roles.png"; ?>" >                                              
+                    </button>
+                    <button style="padding: 0;">
+                        <img src="<?php echo base_url("resources/images/system/")."configuraciones.png"; ?>" >                                              
+                    </button>-->
+        </center>
+                              
     </div>
 </div>
