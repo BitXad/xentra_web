cc851af63c873032e2d0e0084d1e9fcf898f7c7d
alejandro <ale.hv@gmail.com>
2020-03-09 13:04:39 -0400

    ultima factura y categoria egreso CRUD

--- application/controllers/Categoria_egreso.php
+++ application/controllers/Categoria_egreso.php
@@ -4,99 +4,99 @@
  * www.crudigniter.com
  */
  
-class Categoria_ingreso extends CI_Controller{
+class Categoria_Egreso extends CI_Controller{
     function __construct()
     {
         parent::__construct();
-        $this->load->model('Categoria_ingreso_model');
+        $this->load->model('Categoria_egreso_model');
     } 
 
     /*
-     * Listing of categoria_ingreso
+     * Listing of categoria_egreso
      */
     function index()
     {
-        $data['categoria_ingreso'] = $this->Categoria_ingreso_model->get_all_categoria_ingreso();
+        $data['categoria_egreso'] = $this->Categoria_egreso_model->get_all_categoria_egreso();
         
-        $data['_view'] = 'categoria_ingreso/index';
+        $data['_view'] = 'categoria_egreso/index';
         $this->load->view('layouts/main',$data);
-    }
+    } 
 
     /*
-     * Adding a new categoria_ingreso
+     * Adding a new categoria_egreso
      */
     function add()
     {   
         $this->load->library('form_validation');
 
-		$this->form_validation->set_rules('nom_cating','Nom Cating','required');
+		$this->form_validation->set_rules('nom_categr','Nom Cating','required');
 		
 		if($this->form_validation->run())     
         {   
             $params = array(
-				'nom_cating' => $this->input->post('nom_cating'),
-				'id_ging' => $this->input->post('id_ging'),
+				'nom_categr' => $this->input->post('nom_categr'),
+				'id_gegr' => $this->input->post('id_gegr'),
             );
             
-            $categoria_ingreso_id = $this->Categoria_ingreso_model->add_categoria_ingreso($params);
-            redirect('categoria_ingreso/index');
+            $categoria_egreso_id = $this->Categoria_egreso_model->add_categoria_egreso($params);
+            redirect('categoria_egreso/index');
         }
         else
         {            
-            $data['_view'] = 'categoria_ingreso/add';
+            $data['_view'] = 'categoria_egreso/add';
             $this->load->view('layouts/main',$data);
         }
     }  
 
     /*
-     * Editing a categoria_ingreso
+     * Editing a categoria_egreso
      */
-    function edit($id_cating)
+    function edit($id_categr)
     {   
-        // check if the categoria_ingreso exists before trying to edit it
-        $data['categoria_ingreso'] = $this->Categoria_ingreso_model->get_categoria_ingreso($id_cating);
+        // check if the categoria_egreso exists before trying to edit it
+        $data['categoria_egreso'] = $this->Categoria_egreso_model->get_categoria_egreso($id_categr);
         
-        if(isset($data['categoria_ingreso']['id_cating']))
+        if(isset($data['categoria_egreso']['id_categr']))
         {
             $this->load->library('form_validation');
 
-			$this->form_validation->set_rules('nom_cating','Nom Cating','required');
+			$this->form_validation->set_rules('nom_categr','Nom Cating','required');
 		
 			if($this->form_validation->run())     
             {   
                 $params = array(
-					'nom_cating' => $this->input->post('nom_cating'),
-					'id_ging' => $this->input->post('id_ging'),
+					'nom_categr' => $this->input->post('nom_categr'),
+					'id_gegr' => $this->input->post('id_gegr'),
                 );
 
-                $this->Categoria_ingreso_model->update_categoria_ingreso($id_cating,$params);            
-                redirect('categoria_ingreso/index');
+                $this->Categoria_egreso_model->update_categoria_egreso($id_categr,$params);            
+                redirect('categoria_egreso/index');
             }
             else
             {
-                $data['_view'] = 'categoria_ingreso/edit';
+                $data['_view'] = 'categoria_egreso/edit';
                 $this->load->view('layouts/main',$data);
             }
         }
         else
-            show_error('The categoria_ingreso you are trying to edit does not exist.');
+            show_error('The categoria_egreso you are trying to edit does not exist.');
     } 
 
     /*
-     * Deleting categoria_ingreso
+     * Deleting categoria_egreso
      */
-    function remove($id_cating)
+    function remove($id_categr)
     {
-        $categoria_ingreso = $this->Categoria_ingreso_model->get_categoria_ingreso($id_cating);
+        $categoria_egreso = $this->Categoria_egreso_model->get_categoria_egreso($id_categr);
 
-        // check if the categoria_ingreso exists before trying to delete it
-        if(isset($categoria_ingreso['id_cating']))
+        // check if the categoria_egreso exists before trying to delete it
+        if(isset($categoria_egreso['id_categr']))
         {
-            $this->Categoria_ingreso_model->delete_categoria_ingreso($id_cating);
-            redirect('categoria_ingreso/index');
+            $this->Categoria_egreso_model->delete_categoria_egreso($id_categr);
+            redirect('categoria_egreso/index');
         }
         else
-            show_error('The categoria_ingreso you are trying to delete does not exist.');
+            show_error('The categoria_egreso you are trying to delete does not exist.');
     }
     
 }
---
+++ application/views/categoria_egreso/edit.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="col-md-12">
+      	<div class="box box-info">
+            <div class="box-header with-border">
+              	<h3 class="box-title">Categoria Egreso Edit</h3>
+            </div>
+			<?php echo form_open('categoria_egreso/edit/'.$categoria_egreso['id_categr']); ?>
+			<div class="box-body">
+				<div class="row clearfix">
+					<div class="col-md-6">
+						<label for="nom_categr" class="control-label"><span class="text-danger">*</span>Nombre</label>
+						<div class="form-group">
+							<input type="text" name="nom_categr" value="<?php echo ($this->input->post('nom_categr') ? $this->input->post('nom_categr') : $categoria_egreso['nom_categr']); ?>" class="form-control" id="nom_categr" />
+							<span class="text-danger"><?php echo form_error('nom_categr');?></span>
+						</div>
+					</div>
+					<div class="col-md-6 hidden">
+						<label for="id_gegr" class="control-label">Id Ging</label>
+						<div class="form-group">
+							<input type="text" name="id_gegr" value="<?php echo ($this->input->post('id_gegr') ? $this->input->post('id_gegr') : $categoria_egreso['id_gegr']); ?>" class="form-control" id="id_gegr" />
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="box-footer">
+            	<button type="submit" class="btn btn-success">
+					<i class="fa fa-check"></i> Save
+				</button>
+	        </div>				
+			<?php echo form_close(); ?>
+		</div>
+    </div>
+</div>
